634a5bd824936c4769362026a4e6dfb379d11c72
slalli <stefano@dnasoft.nl>
2025-12-31 20:42:00 +0100

    test structure

---
+++ test/bootstrap.js
@@ -0,0 +1,26 @@
+/*
+#################################################################
+#                                                               #
+# Copyright (c) 2022-2025 YottaDB LLC and/or its subsidiaries.  #
+# All rights reserved.                                          #
+#                                                               #
+#   This source code contains the intellectual property         #
+#   of its copyright holder(s), and is made available           #
+#   under a license.  If you do not know the terms of           #
+#   the license, please stop and do not read further.           #
+#                                                               #
+#################################################################
+*/
+
+const {expect} = require('chai');
+const _ = require('lodash');
+
+before(async function () {
+    global.expect = expect;
+
+});
+
+// close browser and reset global variables
+after(async function () {
+});
+
---
+++ test/test_files/dashboard.js
@@ -0,0 +1,23 @@
+/*
+#################################################################
+#                                                               #
+# Copyright (c) 2022-2025 YottaDB LLC and/or its subsidiaries.  #
+# All rights reserved.                                          #
+#                                                               #
+#   This source code contains the intellectual property         #
+#   of its copyright holder(s), and is made available           #
+#   under a license.  If you do not know the terms of           #
+#   the license, please stop and do not read further.           #
+#                                                               #
+#################################################################
+*/
+
+const {expect} = require("chai");
+
+describe("CLIENT: Dashboard: gld file", async () => {
+    it("Test # 30: When gld file is missing", async () => {
+        // we rename first the global file
+        console.log('testing')
+    });
+});
+

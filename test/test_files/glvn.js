292401b85d8b3746f6f1834fc3b3f0489309eff9
slalli <stefano@dnasoft.nl>
2026-01-20 14:56:23 +0100

    TEST: glvn

--- test/test_files/glvn.js
+++ test/test_files/glvn.js
@@ -13,22 +13,128 @@
 const {expect} = require("chai");
 const {createYdbInstance} = require("../utils.cjs");
 
-describe("glvn.hasValue()", async () => {
-    it("", async () => {
+describe("global.hasValue()", async () => {
+    it("test a valid global root", async () => {
         const ydb = await createYdbInstance()
 
-        try {
-            const res = await ydb.db.globals.testGlobal.hasValue()
-            console.log(res)
-
-
-        } catch (err) {
-            console.log(err)
-            expect(err.message).to.have.string('Parameter pid must be a number')
-        }
+        const res = await ydb.db.globals.globalTest.hasValue()
+        expect(res).to.be.true
 
         ydb.disconnect()
     });
 
+    it("test an invalid global root", async () => {
+        const ydb = await createYdbInstance()
 
+        const res = await ydb.db.globals.globalTestEmptyRoot.hasValue()
+        expect(res).to.be.false
+
+        ydb.disconnect()
+    });
+
+    it("test a valid global root", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest._().hasValue()
+        expect(res).to.be.true
+
+        ydb.disconnect()
+    });
+
+    it("test an invalid global root", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTestEmptyRoot._().hasValue()
+        expect(res).to.be.false
+
+        ydb.disconnect()
+    });
+
+    it("test a valid global", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest._(1, 2, 3, 4, 7).hasValue()
+        expect(res).to.be.true
+
+        ydb.disconnect()
+    });
+
+    it("test an invalid global", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest._(1, 2, 3, 4, 5).hasValue()
+        expect(res).to.be.false
+
+        ydb.disconnect()
+    });
 })
+
+describe("global.hasNodes()", async () => {
+    it("test a valid global root", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest.hasNodes()
+        expect(res).to.be.true
+
+        ydb.disconnect()
+    });
+
+    it("test an invalid global root", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTestEmptyRoot.hasNodes()
+        expect(res).to.be.true
+
+        ydb.disconnect()
+    });
+
+    it("test an invalid global", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest._(1, 2, 4).hasNodes()
+        expect(res).to.be.false
+
+        ydb.disconnect()
+    });
+
+    it("test a valid global", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest._(1, 2, 3, 4).hasNodes()
+        expect(res).to.be.true
+
+        ydb.disconnect()
+    });
+})
+
+describe("global.getValue()", async () => {
+    it("test a valid global root number", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest.getValue()
+        expect(typeof res).to.have.string('number')
+
+        ydb.disconnect()
+    });
+
+    it("test a valid global string", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest._(1, 2, 3, 4, 5, 6).getValue()
+        expect(typeof res).to.have.string('string')
+        expect(res).to.have.string('this is a string')
+
+        ydb.disconnect()
+    });
+
+    it("test an invalid global path", async () => {
+        const ydb = await createYdbInstance()
+
+        const res = await ydb.db.globals.globalTest._(1, 2, "this is bad", 4, 5, 6).getValue()
+        expect(typeof res).to.have.string('string')
+        expect(res).to.have.string('')
+
+        ydb.disconnect()
+    });
+})
+

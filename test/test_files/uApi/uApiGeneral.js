f6bc9f8cbbb6c2eb2e72dcac7899b5dc1066e256
slalli <stefano@dnasoft.nl>
2026-02-27 15:23:58 +0100

    docs: improve md's

--- test/test_files/uApi/uApiGeneral.js
+++ test/test_files/uApi/uApiGeneral.js
@@ -133,4 +133,16 @@ describe("uApi methods: test existing function", async () => {
         ydb.disconnect()
     });
 
+    it("badEntryPoint", async () => {
+        const ydb = await createYdbInstance('test-existing')
+
+        try {
+            const res = await ydb.level_1.badEntryPoint('/opt/yottadb')
+
+        } catch (err) {
+            expect(err.message).to.have.string("M code not found")
+        }
+
+        ydb.disconnect()
+    });
 })

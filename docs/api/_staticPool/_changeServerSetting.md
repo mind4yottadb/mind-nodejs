b6be2c65428c9fb255b5246f85ff89b08ad75e5e
slalli <stefano@dnasoft.nl>
2026-06-09 15:16:09 +0200

    docs: _changeServerSettings feat: _changeServerSettings

--- docs/api/_staticPool/_changeServerSetting.md
+++ docs/api/_staticPool/_changeServerSetting.md
@@ -1,5 +1,5 @@
 <!--
-###############################################################
+#################################################################
 #                                                               #
 # Copyright (c) 2026 DnaSoft BV and/or its subsidiaries.        #
 # All rights reserved.                                          #
@@ -12,21 +12,9 @@
 ###############################################################*/
 -->
 
-# Namespace: _staticPool
+### _staticPool._changeServerSetting()
 
 ---
 
-This namespace gives you access to pool statistics and profiling
-
-> This namespace is hidden and FOR INTERNAL USE ONLY
-
----
-
-### Methods:
-
-- [_register(pool)](api/_staticPool/_register.md)
-- [_getPoolStats()](api/_staticPool/_getPoolStats.md)
-- [_changeServerSettings()](api/_staticPool/_changeServerSettings.md)
-
----
+[Back](api/namespace._staticPool.md)
 
--- js/namespaces/_staticPool.js
+++ js/namespaces/_staticPool.js
@@ -75,6 +75,54 @@ class _staticPool {
             })
         })
     }
+
+    _changeServerSetting = function (name, value) {
+        const that = this
+        const RESP3 = that.objRoot.RESP3
+
+        return new Promise(function (resolve, reject) {
+            if (that.connected === false || that.loggedIn === false) {
+                reject(new Error(errors.NOT_LOGGED_IN + 'Not logged in'))
+            }
+
+            if (typeof name !== 'string') {
+                reject(new Error(errors.PARAM_NOT_STRING + 'name must be a string'))
+
+                return
+            }
+
+            if (name === '') {
+                reject(new Error(errors.PARAM_NOT_STRING_OR_EMPTY + 'name must be a string longer than 0 chars'))
+
+                return
+            }
+
+            if (typeof value !== 'number') {
+                reject(new Error(errors.PARAM_NOT_NUMBER + 'value must be a number'))
+
+                return
+            }
+
+            // send command
+            const opCode = 'pool.changeServerSetting'
+
+            that.writer("*3" + RESP3.CRLF +
+                RESP3.build.blob(opCode) +
+                RESP3.build.blob(name) +
+                RESP3.build.blob(value)
+            );
+
+            that.reader(data => {
+                if (data.charAt(0) === '-') {
+                    reject(new Error(RESP3.parse.simpleError(data)))
+
+                    return
+                }
+
+                resolve()
+            })
+        })
+    }
 }
 
 module.exports = _staticPool

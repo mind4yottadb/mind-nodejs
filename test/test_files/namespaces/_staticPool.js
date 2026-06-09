6d0adfb44cfe2480cf9b9e69a10e3f144c14acb5
slalli <stefano@dnasoft.nl>
2026-06-09 14:53:55 +0200

    test: _staticPool

--- test/test_files/namespaces/_staticPool.js
+++ test/test_files/namespaces/_staticPool.js
@@ -22,6 +22,8 @@ describe("_staticPool.", async () => {
 
         await pool.create('127.0.0.1', 10000, 'admin', 'admin', {})
 
+        expect(pool.guid !== '').to.be.true
+
         ydb.db.globals.addName('_mindPools')
 
         const val = await ydb.db.globals._mindPools._(pool.guid, "pids").findNext()
@@ -32,7 +34,49 @@ describe("_staticPool.", async () => {
         ydb.disconnect()
     });
 
-    it("_staticPool.getPoolStats()", async function () {
+    it("_staticPool.getPoolStats(): 2 procs", async function () {
+        this.timeout(20000)
+
+        const pool = new mindServer.staticPool(2)
+
+        await pool.create('127.0.0.1', 10000, 'admin', 'admin', {})
+
+        const val = await pool.devOps.getPoolStats()
+
+        expect(val.length).to.equal(2)
+
+        pool.destroy()
+    });
+
+    it("_staticPool.getPoolStats(): 8 procs", async function () {
+        this.timeout(20000)
+
+        const pool = new mindServer.staticPool(8)
+
+        await pool.create('127.0.0.1', 10000, 'admin', 'admin', {})
+
+        const val = await pool.devOps.getPoolStats()
+
+        expect(val.length).to.equal(8)
+
+        pool.destroy()
+    });
+
+    it("_staticPool.getPoolStats(): 16 procs + extension", async function () {
+        this.timeout(20000)
+
+        const pool = new mindServer.staticPool(16, 2)
+
+        await pool.create('127.0.0.1', 10000, 'admin', 'admin', {})
+
+        const val = await pool.devOps.getPoolStats()
+
+        expect(val.length).to.equal(16)
+
+        pool.destroy()
+    });
+
+    it("_staticPool.getPoolStats(): 32 procs", async function () {
         this.timeout(20000)
 
         const pool = new mindServer.staticPool(32)
@@ -44,6 +88,30 @@ describe("_staticPool.", async () => {
         expect(val.length).to.equal(32)
 
         pool.destroy()
+    });
 
+    it("_staticPool.getPoolStats(): validate all fields", async function () {
+        this.timeout(20000)
+
+        const pool = new mindServer.staticPool(2)
+
+        await pool.create('127.0.0.1', 10000, 'admin', 'admin', {})
+
+        const ret = await pool.devOps.getPoolStats()
+        expect(ret[0].cpu.cstime !== undefined).to.be.true
+        expect(ret[0].cpu.cutime !== undefined).to.be.true
+        expect(ret[0].cpu.stime !== undefined).to.be.true
+        expect(ret[0].cpu.utime !== undefined).to.be.true
+
+        expect(ret[0].memory.VmHWM !== undefined).to.be.true
+        expect(ret[0].memory.VmLck !== undefined).to.be.true
+        expect(ret[0].memory.VmPeak !== undefined).to.be.true
+        expect(ret[0].memory.VmRss !== undefined).to.be.true
+        expect(ret[0].memory.VmSize !== undefined).to.be.true
+
+        expect(ret[0].pid !== undefined).to.be.true
+        expect(ret[0].state !== undefined).to.be.true
+
+        pool.destroy()
     });
 })

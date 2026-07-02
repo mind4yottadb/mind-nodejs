26f87d72cca1a7c257aaac485a98c4b399aba214
slalli <stefano@dnasoft.nl>
2026-07-03 00:14:45 +0200

    test: test action

--- js/login.js
+++ js/login.js
@@ -15,7 +15,8 @@ const fs = require("fs");
 const errors = require("./errors");
 
 const driverName = 'mind4yottadb.js'
-const driverVersion = JSON.parse(fs.readFileSync(__dirname.substring(0, __dirname.lastIndexOf('\\')) + '/package.json', 'utf8')).version
+//const driverVersion = JSON.parse(fs.readFileSync(__dirname.substring(0, __dirname.lastIndexOf('\\')) + '/package.json', 'utf8')).version
+const driverVersion = 'test-version'
 const driverDescription = 'MIND for YottaDB node.js driver'
 
 module.exports = async function (that, writer, reader, resolve, reject, username, password, options) {

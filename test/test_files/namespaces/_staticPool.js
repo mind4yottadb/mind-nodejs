0d8ec9347b4e41a3e52595abb54f95689606e42f
slalli <stefano@dnasoft.nl>
2026-07-23 15:55:08 +0200

    fix: doggy test

--- test/test_files/namespaces/_staticPool.js
+++ test/test_files/namespaces/_staticPool.js
@@ -27,7 +27,7 @@ describe("_staticPool.", async () => {
         ydb.db.globals.addName('_mindSessions')
 
         const val = await ydb.db.globals._mindSessions._('pools', pool.guid, "pids").findNext()
-        expect(typeof val).to.have.string('number')
+        expect(typeof val).to.have.string('string')
 
         pool.destroy()
 
